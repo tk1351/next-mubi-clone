a80e96f353d63be4b7430e1e3563fa97276aa4bf
tk1351 <takakikitsui0111@gmail.com>
2023-12-18 13:34:22 +0900

    chore: add custom css to preview.ts

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,8 @@
 import type { Preview } from "@storybook/react";
 
+import "../app/reset.css";
+import "../app/globals.css";
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
